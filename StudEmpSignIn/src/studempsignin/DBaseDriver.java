edbbe6578eb9ee594b45d7df71d98295e663ad6d
mr-robot9 <kheneahm9@yahoo.com>
2016-07-21 20:31:47 -0500

    First commit

--- StudEmpSignIn/src/studempsignin/DBaseDriver.java
+++ StudEmpSignIn/src/studempsignin/DBaseDriver.java
@@ -37,97 +37,98 @@ public class DBaseDriver {
             connect.close();
         }
     }
-
-    public void printTest(String empID) throws SQLException {
-        
-        //create scanner
-//        Scanner input = new Scanner(System.in);
-//        String empID;
-        
-        while(true){
-        //prompt user to enter ID
-//        System.out.println("Enter Employee ID: ");
-//        empID = input.next();
-        
-        //check if the ID is valid
-        boolean isIDInDatabase = checkDatabase(empID);
-        
-        if(isIDInDatabase == false){
-            System.out.println("Enter valid ID!");
-        }
-        else{
-            break;
-        }
-        }
-
-        
-        //create statement
-        Statement statement = connect.createStatement();
-        
-        //convert stringID to string to make the query valid!
-        
-        //holds the result of the executed query
-        ResultSet resultSet = statement.executeQuery("select EmployeeID, FirstName, LastName from emp_info where EmployeeID "
-                + "= '" + empID + "'");
-
-        //iterate through the result and print the statements
-        while (resultSet.next()) {
-            System.out.println("Employee ID: " + resultSet.getString(1) + " is "
-                    + resultSet.getString(2) + " " + resultSet.getString(3));
-        }
-
-        connect.close();
-
-    }
-
-    //method to check database for empID
-    private boolean checkDatabase(String empID) throws SQLException{
-        boolean isInDatabase = false;
-        int i = 0;
-        int inArray = 0;
-        String[] empIDs = getEmpIDFromDatabase();
-        
-        while(i < empIDs.length){
-            if(empID.equals(empIDs[i])){
-                 inArray++;
-            }
-            i++;
-        }
-        if(inArray != 0){
-            isInDatabase = true;
-              }
-        return isInDatabase;
-    }
-
-    private String[] getEmpIDFromDatabase() throws SQLException {
-        Set<String> arraySet = new HashSet<String>();
-        String[] arrayID;
-         Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/AUStudentEmployees",
-                                "kheneahm", "kennygoham");
-         
-         String query = "select EmployeeID from emp_info;";
-         
-         Statement stmnt = connect.createStatement();
-         
-         ResultSet rslt = stmnt.executeQuery(query);
-         
-         while(rslt.next()){
-             arraySet.add(rslt.getString("EmployeeID")); // grabs ID from database
-         }
-         
-         stmnt.close();
-         
-         //returns iterator from arraySet
-         Iterator<String> iterator = arraySet.iterator();
-         
-         //create an array with a fixed size
-         arrayID = new String[arraySet.size()];
-         int i = 0;
-         while(iterator.hasNext()){
-             arrayID[i] = iterator.next();
-             i++;
-        
-            }
-         return arrayID;
-    }
+//
+    //These three methods were for testing purposes...now integerated in SignIn_Controller
+//    public void printTest(String empID) throws SQLException {
+//        
+//        //create scanner
+////        Scanner input = new Scanner(System.in);
+////        String empID;
+//        
+//        while(true){
+//        //prompt user to enter ID
+////        System.out.println("Enter Employee ID: ");
+////        empID = input.next();
+//        
+//        //check if the ID is valid
+//        boolean isIDInDatabase = checkDatabase(empID);
+//        
+//        if(isIDInDatabase == false){
+//            System.out.println("Enter valid ID!");
+//        }
+//        else{
+//            break;
+//        }
+//        }
+//
+//        
+//        //create statement
+//        Statement statement = connect.createStatement();
+//        
+//        //convert stringID to string to make the query valid!
+//        
+//        //holds the result of the executed query
+//        ResultSet resultSet = statement.executeQuery("select EmployeeID, FirstName, LastName from emp_info where EmployeeID "
+//                + "= '" + empID + "'");
+//
+//        //iterate through the result and print the statements
+//        while (resultSet.next()) {
+//            System.out.println("Employee ID: " + resultSet.getString(1) + " is "
+//                    + resultSet.getString(2) + " " + resultSet.getString(3));
+//        }
+//
+//        connect.close();
+//
+//    }
+//
+//    //method to check database for empID
+//    private boolean checkDatabase(String empID) throws SQLException{
+//        boolean isInDatabase = false;
+//        int i = 0;
+//        int inArray = 0;
+//        String[] empIDs = getEmpIDFromDatabase();
+//        
+//        while(i < empIDs.length){
+//            if(empID.equals(empIDs[i])){
+//                 inArray++;
+//            }
+//            i++;
+//        }
+//        if(inArray != 0){
+//            isInDatabase = true;
+//              }
+//        return isInDatabase;
+//    }
+//
+//    private String[] getEmpIDFromDatabase() throws SQLException {
+//        Set<String> arraySet = new HashSet<String>();
+//        String[] arrayID;
+//         Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/AUStudentEmployees",
+//                                "kheneahm", "kennygoham");
+//         
+//         String query = "select EmployeeID from emp_info;";
+//         
+//         Statement stmnt = connect.createStatement();
+//         
+//         ResultSet rslt = stmnt.executeQuery(query);
+//         
+//         while(rslt.next()){
+//             arraySet.add(rslt.getString("EmployeeID")); // grabs ID from database
+//         }
+//         
+//         stmnt.close();
+//         
+//         //returns iterator from arraySet
+//         Iterator<String> iterator = arraySet.iterator();
+//         
+//         //create an array with a fixed size
+//         arrayID = new String[arraySet.size()];
+//         int i = 0;
+//         while(iterator.hasNext()){
+//             arrayID[i] = iterator.next();
+//             i++;
+//        
+//            }
+//         return arrayID;
+//    }
 }
